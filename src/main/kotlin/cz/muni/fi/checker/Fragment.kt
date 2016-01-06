b8b85c21e9c5bb1ba1c0840315a4a0fbd8998899
Samuel Pastva <daemontus@gmail.com>
2016-01-06 14:28:52 +0100

    Kripke Fragment and related classes.

---
+++ src/main/kotlin/cz/muni/fi/checker/Fragment.kt
@@ -0,0 +1,92 @@
+package cz.muni.fi.checker
+
+import cz.muni.fi.ctl.Atom
+
+/**
+ * Represents a Node (empty for now, reserved for future use)
+ */
+public interface Node { }
+
+/**
+ * Represents an immutable set of colors (parameters) of the model.
+ * All operations should behave as normal mathematical set.
+ *
+ * The relation of equivalence does not have to satisfy the set semantics,
+ * since these sets are often symbolic and can't be compared easily or at all.
+ * (Main algorithm does not require it, but it can be used during testing,
+ * so if you want to use the default test suite, you have to implement some sort
+ * of equivalence)
+ */
+public interface Colors<C> {
+
+    operator fun minus(other: C): C
+
+    operator fun plus(other: C): C
+
+    infix fun intersect(other: C): C
+
+    infix fun union(other: C): C = this + other
+
+    infix fun subtract(other: C): C = this - other
+
+    fun isEmpty(): Boolean
+
+    fun isNotEmpty(): Boolean = !isEmpty()
+
+}
+
+public interface PartitionFunction<N: Node> {
+
+    /**
+     * Get ID of partition which owns given node.
+     */
+    val ownerId: N.() -> Int
+
+    /**
+     * My partition ID.
+     */
+    val myId: Int
+
+}
+
+
+/**
+ * Note: Several invariants should hold for each valid Kripke fragment
+ * 1. validNodes should always return subset of allNodes (with respect to colors).
+ * 2. for every node N and color C from allNodes, successors(N) should be non empty (no finite paths).
+ * 3. Border state set is a union of all successors/predecessors minus all nodes (with respect to colors).
+ * 4. If N is successor of M for colors C, M is predecessor of N for colors C.
+ * 5. The default value in all node sets is always an empty color space.
+ */
+public interface KripkeFragment<N: Node, C: Colors<C>> {
+
+    /**
+     * Find all successors of given node. (Even in different fragments)
+     */
+    val successors: N.() -> Nodes<N, C>
+
+    /**
+     * Find all predecessors of given node. (Even in different fragments)
+     */
+    val predecessors: N.() -> Nodes<N, C>
+
+    /**
+     * Map of all (non border) nodes of the fragment with colors for which they are valid
+     */
+    fun allNodes(): Nodes<N, C>
+
+    /**
+     * Find all nodes (and respective colors) where given atomic proposition holds in this fragment.
+     */
+    fun validNodes(a: Atom): Nodes<N, C>
+
+}
+
+/**
+ * Utility data class
+ */
+public data class Edge<N: Node, C: Colors<C>>(
+        public val start: N,
+        public val end: N,
+        public val colors: C
+)
---
+++ src/main/kotlin/cz/muni/fi/checker/Nodes.kt
@@ -0,0 +1,162 @@
+package cz.muni.fi.checker
+
+import java.util.*
+
+/**
+ * Although kotlin provides a map with default value, this explicit data
+ * structure enables us to check for it's presence at compile time and
+ * has more convenient semantics.
+ */
+
+/**
+ * A read only map with default value.
+ *
+ * The equivalence relation on this class does not have to be implemented reliably, since
+ * a lot of color sets are represented symbolically and therefore direct comparison is
+ * often unfeasible.
+ *
+ * Therefore avoid using equals/hashCode with NodeSet.
+ *
+ * NodeSet should be immutable and therefore thread safe.
+ */
+public interface Nodes<N: Node, C: Colors<C>> {
+
+    /**
+     * Default element returned
+     */
+    public val emptyColors: C
+
+    /**
+     * All keys that have their value set.
+     */
+    public val validKeys: Set<N>
+
+    /**
+     * All entries that are not empty.
+     */
+    public val validEntries: Set<Map.Entry<N, C>>
+
+    /**
+     * Return color set for given key or empty set if no value is set.
+     */
+    operator fun get(key: N): C
+
+    operator fun plus(other: Nodes<N, C>): Nodes<N, C>
+
+    operator fun minus(other: Nodes<N, C>): Nodes<N, C>
+
+    infix fun intersect(other: Nodes<N, C>): Nodes<N, C>
+
+    infix fun union(other: Nodes<N, C>): Nodes<N, C> = this + other
+
+    infix fun subtract(other: Nodes<N, C>): Nodes<N, C> = this - other
+
+}
+
+/**
+ * Mutable variant of node set.
+ */
+public interface MutableNodes<N: Node, C: Colors<C>>: Nodes<N, C> {
+
+    /**
+     * When updating elements, union is created instead of replacing old value.
+     * Therefore, mutable node set can only grow.
+     *
+     * Returns true if something has been added to the set.
+     */
+    fun putOrUnion(key: N, value: C): Boolean
+
+    /**
+     * Make immutable copy
+     */
+    fun toNodes(): Nodes<N, C>
+}
+
+public open class MapNodes<N: Node, C: Colors<C>>(
+        private val map: Map<N, C>, //TODO: Shouldn't we make a defensive copy of this?
+        public override val emptyColors: C
+) : Nodes<N, C> {
+
+    override val validKeys: Set<N> = map.keys
+    override val validEntries: Set<Map.Entry<N, C>> = map.entries
+
+    override fun get(key: N): C = map.getOrElse(key) { emptyColors }
+
+    override fun plus(other: Nodes<N, C>): Nodes<N, C> {
+        val new = HashMap(map)
+        for ((k, v) in other.validEntries) {
+            new[k] = get(k) + v
+        }
+        return MapNodes(new, emptyColors)
+    }
+
+    override fun minus(other: Nodes<N, C>): Nodes<N, C> {
+        val new = HashMap(map)
+        for ((k, v) in other.validEntries) {
+            new[k] = get(k) - v
+        }
+        return MapNodes(new.filterValues { it.isNotEmpty() }, emptyColors)
+    }
+
+    override fun intersect(other: Nodes<N, C>): Nodes<N, C> {
+        val new = HashMap(map)
+        for ((k, v) in other.validEntries) {
+            new[k] = get(k) intersect v
+        }
+        return MapNodes(new.filterValues { it.isNotEmpty() }, emptyColors)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other is MapNodes<*, *>) {
+            return this.emptyColors == other.emptyColors && this.map == other.map
+        } else return false
+    }
+
+    override fun hashCode(): Int {
+        return this.map.hashCode() + 31 * emptyColors.hashCode()
+    }
+
+    override fun toString(): String {
+        return "NodeSet(default=$emptyColors, values=$map)"
+    }
+
+}
+
+public class MutableMapNodes<N: Node, C: Colors<C>>(
+    private val map: MutableMap<N, C>,
+    emptyColors: C
+): MapNodes<N, C>(map, emptyColors), MutableNodes<N, C> {
+
+    override fun toNodes(): Nodes<N, C> {
+        synchronized(map) {
+            return MapNodes(HashMap(map), emptyColors)
+        }
+    }
+
+    override fun putOrUnion(key: N, value: C): Boolean {
+        if (value.isEmpty()) return false   //fail fast
+        synchronized(map) {
+            if (!map.containsKey(key)) {    //skip subtraction test
+                map.put(key, value)
+                return true
+            }
+            val diff = value - map[key]!!
+            if (diff.isEmpty()) {   //current value is superset of new value, skip!
+                return false
+            } else {
+                map[key] = value + map[key]!!
+                return true
+            }
+        }
+    }
+
+}
+
+public fun <N: Node, C: Colors<C>> Map<N, C>.toNodes(value: C): Nodes<N, C>
+        = MapNodes(this, value)
+public fun <N: Node, C: Colors<C>> MutableMap<N, C>.toMutableNodes(value: C): MutableNodes<N, C>
+        = MutableMapNodes(this, value)
+
+public fun <N: Node, C: Colors<C>> nodesOf(default: C, vararg pairs: Pair<N, C>): Nodes<N, C>
+        = MapNodes(pairs.toMap({it.first}, {it.second}), default)
+
