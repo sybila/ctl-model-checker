c49b7e0ecf19c13a272d07525f9c763d8534885c
Samuel Pastva <daemontus@gmail.com>
2017-05-03 16:02:45 +0200

    fix bit set solver transfer size

--- src/main/kotlin/com/github/sybila/checker/solver/BitSetSolver.kt
+++ src/main/kotlin/com/github/sybila/checker/solver/BitSetSolver.kt
@@ -27,7 +27,7 @@ class BitSetSolver(
 
     override fun BitSet.minimize() {}
 
-    override fun BitSet.byteSize(): Int =  4 + this.toLongArray().size
+    override fun BitSet.byteSize(): Int =  4 + this.toLongArray().size * 8
 
     override fun ByteBuffer.putColors(colors: BitSet): ByteBuffer = this.apply {
         val array = colors.toLongArray()
