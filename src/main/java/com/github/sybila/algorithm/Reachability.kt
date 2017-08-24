4b42fd809f16b90524a2b57cb1966e8b9a94dcad
Samuel Pastva <daemontus@gmail.com>
2017-08-24 09:58:06 +0200

    inline chunk dispenser

--- src/main/java/com/github/sybila/algorithm/Reachability.kt
+++ src/main/java/com/github/sybila/algorithm/Reachability.kt
@@ -11,7 +11,9 @@ import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.Deferred
 import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.channels.consumeEach
+import kotlinx.coroutines.experimental.channels.produce
 import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.coroutines.experimental.CoroutineContext
 import kotlin.system.measureTimeMillis
 
@@ -21,13 +23,13 @@ interface Reachability<S : Any, P : Any> : Algorithm<S, P>, StateMapContext<S, P
         = lazyAsync {
         val reach = reachJob.await()
         val result = reach.toMutable()
-        val chunks = ChunkDispenser(meanChunkTime)
+        //val chunks = ChunkDispenser(meanChunkTime)
 
         //TODO: Search queue heuristic?
         var recompute: List<Pair<S, S?>> = reach.states.map { it to null }.toList()
 
         while (recompute.isNotEmpty()) {
-            val changed = recompute.mapChunksInline(algorithm = this@Reachability, fork = fork, executor = executor, chunkDispenser = chunks, action = { (state, dep) ->
+            val changed = recompute.mapChunksInline(algorithm = this@Reachability, fork = fork, executor = executor, meanChunkTime = meanChunkTime, action = { (state, dep) ->
                 if (dep == null) state else {
                     state.takeIf { result.increaseKey(state, transitionBound(state, dep, time) and result[dep]) }
                 }
@@ -73,9 +75,48 @@ inline suspend fun <T, R, P:Any> List<T>.mapChunksInline(
         crossinline action: Solver<P>.(T) -> R?,
         fork: Int = 1,
         executor: CoroutineContext = CommonPool,
-        chunkDispenser: ChunkDispenser = ChunkDispenser()
+        meanChunkTime: Long
 ) : List<R?> {
-    val list = this
+    val chunkSize = AtomicInteger(1)
+    // what a nice warning you have here...
+    val result = Arrays.asList<R?>(*(arrayOfNulls<Any?>(this.size) as Array<out R>))
+    val original = this@mapChunksInline
+    val chunks = produce<IntRange>(executor) {
+        var chunkStart = 0  //inclusive
+        while (chunkStart != original.size) {
+            val chunk = chunkSize.get()
+            val chunkEnd = Math.min(original.size, chunkStart + chunk)  //exclusive
+            send(chunkStart until chunkEnd)
+            chunkStart = chunkEnd
+        }
+        original.take(10)
+        close()
+    }
+    (1..fork).map {
+        async(executor) {
+            chunks.consumeEach { items ->
+                val solver = algorithm.solver
+                val elapsed = measureTimeMillis {
+                    items.forEach {
+                        result[it] = solver.action(original[it])
+                    }
+                }
+                if (elapsed < 0.8 * meanChunkTime || elapsed > 1.2 * meanChunkTime) {
+                    val chunk = items.last - items.first + 1    // + 1 for inclusive range
+                    val itemTime = elapsed / chunk.toDouble()
+                    if (itemTime == 0.0) {
+                        // If we are real fast, we can get a zero elapsed time and hence a zero item time
+                        chunkSize.set(2 * chunk)    // in which case, just increase the chunk arbitrarily.
+                    } else {
+                        // On the other hand, we can also be really slow and get a zero chunk size
+                        chunkSize.set(Math.max(1, (meanChunkTime / itemTime).toInt()))  // hence use a lower bound.
+                    }
+                }
+            }
+        }
+    }.forEach { it.await() }
+    return result
+    /*val list = this
     val result = ArrayList<R?>(list.size).apply { (0 until list.size).forEach { add(null) }  }
     val chunks = this.chunks(executor, chunkDispenser)
     (1..fork).map {
@@ -92,5 +133,5 @@ inline suspend fun <T, R, P:Any> List<T>.mapChunksInline(
             }
         }
     }.map { it.await() }
-    return result
+    return result*/
 }
