21eb0ab2093ba267d918f74d93490a164364b0f9
Samuel Pastva <daemontus@gmail.com>
2017-08-25 15:30:16 +0200

    component benchmark

--- src/main/java/com/github/sybila/Fun.kt
+++ src/main/java/com/github/sybila/Fun.kt
@@ -1,12 +1,19 @@
 package com.github.sybila
 
+import com.github.sybila.algorithm.makeDeferred
 import com.github.sybila.collection.ArrayCollectionContext
+import com.github.sybila.collection.ArrayStateMap
+import com.github.sybila.collection.SingletonStateMap
 import com.github.sybila.funn.ModelChecker
 import com.github.sybila.funn.ode.ODETransitionSystem
+import com.github.sybila.huctl.dsl.*
 import com.github.sybila.huctl.parser.readHUCTLp
 import com.github.sybila.ode.model.Parser
+import com.github.sybila.ode.model.computeApproximation
 import com.github.sybila.solver.grid.Grid2
+import kotlinx.coroutines.experimental.runBlocking
 import java.io.File
+import java.io.PrintStream
 import java.util.*
 import kotlin.system.measureTimeMillis
 
@@ -28,7 +35,7 @@ fun main(args: Array<String>) {
 
     println("Test run: ${measureTimeMillis { action() }}")
 
-    val results = ArrayList<Long>()
+    /*val results = ArrayList<Long>()
     repeat(5) {
 
         val elapsed = measureTimeMillis {
@@ -40,16 +47,17 @@ fun main(args: Array<String>) {
         results.add(elapsed)
     }
 
-    println("Avr.: ${results.sum() / 5}")
+    println("Avr.: ${results.sum() / 5}")*/
 
 }
 
 fun action() {
+    //val mFile = File("/Users/daemontus/Downloads/model_indep.bio")
     val mFile = File("model.bio")
     val pFile = File("prop.ctl")
     //val oFile = File("/Users/daemontus/Downloads/fun_result.json")
 
-    val model = Parser().parse(mFile)
+    val model = Parser().parse(mFile).computeApproximation(fast = false)
     val prop = readHUCTLp(pFile, onlyFlagged = true)
 
     println(prop)
@@ -61,9 +69,19 @@ fun action() {
     }
     val mc = ModelChecker(model = tSystem, maps = maps, solver = tSystem.solver, fork = 4, meanChunkTime = 25)
 
-    val s = prop["stay_high"]!!
+    val result = runBlocking {
+        val counter = mc.makeTerminalComponents().await()
+        println("Found ${counter.min}..${counter.max} components.")
+        (counter.min..counter.max).mapNotNull { it ->
+            counter[it]?.let { p ->
+                "C$it" to SingletonStateMap<Int, Grid2>(0, p)
+            }
+        }.toMap()
+    }
 
-    val result = mc.check(prop.map { it.key to it.value })
+    //val result = mc.check(prop.map { it.key to it.value })
+
+    //val s = prop["stay_high"]!!
     //val result = mc.check(listOf("stay_high" to s))
 
     /*PrintStream(oFile.apply { this.createNewFile() }.outputStream()).use { outStream ->
